e87a7b50a2979f89e4fc77c26cb88ee7f9f98f74
joanvila <vila.joan94@gmail.com>
2016-10-30 17:00:56 +0100

    Example raml moved to example folder

--- examples/server.js
+++ examples/server.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
-var cors = require('cors');
 
 var app = express();
-app.use(cors());
 
 app.use(bodyParser.json());
 
