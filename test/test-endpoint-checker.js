84dd93542406ee3a50712fa0037fdfe820d28163
joanvila <vila.joan94@gmail.com>
2016-11-01 22:16:38 +0100

    Mocking http requests

--- test/test-endpoint-checker.js
+++ test/test-endpoint-checker.js
@@ -5,25 +5,55 @@ const chai = require('chai');
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
+const sinon = require('sinon');
+
+const request = require('request');
 const EndpointChecker = require('../lib/endpoint-checker');
 
 describe('endpoint-checker', () => {
-    describe('check()', () => {
 
-        const endpointChecker = new EndpointChecker();
+    const endpointChecker = new EndpointChecker();
+
+    describe('check() good endpoint', () => {
+
+        before(() => {
+            sinon
+                .stub(request, 'get')
+                .yields(null, {statusCode: 200}, {});
+        });
+
+        after(() => {
+            request.get.restore();
+        });
+
 
         it('should accept promise on 200 code', () => {
             return expect(endpointChecker.check('http://www.google.com')).to.eventually
               .equal('Resource OK');
         });
 
-        it('should reject promise with error on non 200 code', () => {
+    });
+
+    describe('check() bad endpoint', () => {
+
+        before(() => {
+            sinon
+                .stub(request, 'get')
+                .yields(null, {statusCode: 500}, {});
+        });
+
+        after(() => {
+            request.get.restore();
+        });
+
+        it('should reject promise with error', () => {
             return expect(endpointChecker.check('http://localhost:0')).to.eventually
               .be.rejectedWith('Received a non 200 error code')
               .and.be.an.instanceOf(Error);
         });
 
     });
+
 });
 
 
