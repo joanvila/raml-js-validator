eb0c4e1a99f047e32836bf30a5ddbd22f7b22420
joanvila <vila.joan94@gmail.com>
2016-11-04 23:31:49 +0100

    Tests passing

--- test/test-endpoint-builder.js
+++ test/test-endpoint-builder.js
@@ -3,24 +3,26 @@
 const assert = require('chai').assert;
 const endpointBuilder = require('../lib/endpoint-builder');
 
+// TODO: Test the parameters
+
 describe('endpoint-builder', () => {
     describe('buildBaseEndpoint()', () => {
 
         it('should return localhost when no target specified', () => {
             assert.equal(
-                endpointBuilder.buildBaseEndpoint(undefined, '/resource'),
+                endpointBuilder.buildBaseEndpoint(undefined, '/resource', []),
                 'http://localhost:8080/resource');
         });
 
         it('should return the target specified plus the resource', () => {
             assert.equal(
-                endpointBuilder.buildBaseEndpoint('http://target', '/resource'),
+                endpointBuilder.buildBaseEndpoint('http://target', '/resource', []),
                 'http://target/resource');
         });
 
         it('should strip the ending slash on the target', () => {
             assert.equal(
-                endpointBuilder.buildBaseEndpoint('http://target/', '/resource'),
+                endpointBuilder.buildBaseEndpoint('http://target/', '/resource', []),
                 'http://target/resource');
         });
 
