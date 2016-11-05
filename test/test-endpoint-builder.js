4dfc791a50f98edb0414080d9ba6f1587397f30f
joanvila <vila.joan94@gmail.com>
2016-11-05 17:22:48 +0100

    Target host overriding baseuri

--- test/test-endpoint-builder.js
+++ test/test-endpoint-builder.js
@@ -7,29 +7,50 @@ const paramMappings = require('../lib/param-mappings');
 const sinon = require('sinon');
 
 describe('endpoint-builder', () => {
-    describe('buildWithoutParameters()', () => {
+    describe('buildUnparsedUri()', () => {
 
-        it('should return localhost when no target specified', () => {
-            assert.equal(
-                endpointBuilder.build(undefined, '/resource', []),
-                'http://localhost:8080/resource');
-        });
+        let mockResourceNoBaseuri = {
+            absoluteUri: function() {return '/resource'},
+            completeRelativeUri: function() {return '/resource'}
+        }
 
-        it('should return the target specified plus the resource', () => {
-            assert.equal(
-                endpointBuilder.build('http://target', '/resource', []),
-                'http://target/resource');
-        });
+        let mockResourceWithBaseuri = {
+            absoluteUri: function() {return 'http://baseuri/resource'},
+            completeRelativeUri: function() {return '/resource'}
+        }
 
         it('should strip the ending slash on the target', () => {
             assert.equal(
-                endpointBuilder.build('http://target/', '/resource', []),
+                endpointBuilder.buildUnparsedUri('http://target/', mockResourceNoBaseuri),
                 'http://target/resource');
         });
 
+        it('target specified and no baseUri in the resource', () => {
+            assert.equal(
+                endpointBuilder.buildUnparsedUri('http://target', mockResourceNoBaseuri),
+                'http://target/resource');
+        });
+
+        it('target specified and baseUri in the resource', () => {
+            assert.equal(
+                endpointBuilder.buildUnparsedUri('http://target', mockResourceWithBaseuri),
+                'http://target/resource');
+        });
+
+        it('target not specified and no baseUri in the resource', () => {
+            assert.equal(
+                endpointBuilder.buildUnparsedUri(undefined, mockResourceNoBaseuri),
+                'http://localhost:8080/resource');
+        });
+
+        it('target not specified and baseUri in the resource', () => {
+            assert.equal(
+                endpointBuilder.buildUnparsedUri(undefined, mockResourceWithBaseuri),
+                'http://baseuri/resource');
+        });
     });
 
-    describe('buildWithStringParameters()', () => {
+    describe('parseUriWithStringParameters()', () => {
 
         before(() => {
             sinon.stub(paramMappings, 'get', function() { return 'a'; });
@@ -43,19 +64,21 @@ describe('endpoint-builder', () => {
 
         it('should return a parsed uri with a param replaced', () => {
             assert.equal(
-                endpointBuilder.build(undefined, '/resource/{testParam}', [mockParam]),
-                'http://localhost:8080/resource/a');
+                endpointBuilder.parseUriWithParameters(
+                    'http://target.com/resource/{testParam}', [mockParam]),
+                'http://target.com/resource/a');
         });
 
         it('should return a parsed uri with more than one param replaced', () => {
             assert.equal(
-                endpointBuilder.build(undefined, '/resource/{testParam}/{anotherTestParam}', [mockParam, mockParam]),
-                'http://localhost:8080/resource/a/a');
+                endpointBuilder.parseUriWithParameters(
+                    'http://target.com/resource/{testParam}/{anotherTestParam}', [mockParam, mockParam]),
+                'http://target.com/resource/a/a');
         });
 
     });
 
-    describe('buildWithIntParameters()', () => {
+    describe('parseUriWithIntParameters()', () => {
 
         before(() => {
             sinon.stub(paramMappings, 'get', function() { return 1; });
@@ -69,8 +92,9 @@ describe('endpoint-builder', () => {
 
         it('should return a parsed uri with an integer type param', () => {
             assert.equal(
-                endpointBuilder.build(undefined, '/resource/{testParam}/{anotherTestParam}', [mockParam, mockParam]),
-                'http://localhost:8080/resource/1/1');
+                endpointBuilder.parseUriWithParameters(
+                    'http://target.com/resource/{testParam}/{anotherTestParam}', [mockParam, mockParam]),
+                'http://target.com/resource/1/1');
         });
 
     });
