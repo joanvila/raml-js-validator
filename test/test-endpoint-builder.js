96c9ee10e4c95456d13ac88dabbe57391ffa17fb
joanvila <vila.joan94@gmail.com>
2016-11-05 12:06:14 +0100

    Request with one parameter tested

--- test/test-endpoint-builder.js
+++ test/test-endpoint-builder.js
@@ -3,28 +3,50 @@
 const assert = require('chai').assert;
 const endpointBuilder = require('../lib/endpoint-builder');
 
-// TODO: Test the parameters
+const paramMappings = require('../lib/param-mappings');
+const sinon = require('sinon');
 
 describe('endpoint-builder', () => {
-    describe('buildBaseEndpoint()', () => {
+    describe('buildWithoutParameters()', () => {
 
         it('should return localhost when no target specified', () => {
             assert.equal(
-                endpointBuilder.buildBaseEndpoint(undefined, '/resource', []),
+                endpointBuilder.build(undefined, '/resource', []),
                 'http://localhost:8080/resource');
         });
 
         it('should return the target specified plus the resource', () => {
             assert.equal(
-                endpointBuilder.buildBaseEndpoint('http://target', '/resource', []),
+                endpointBuilder.build('http://target', '/resource', []),
                 'http://target/resource');
         });
 
         it('should strip the ending slash on the target', () => {
             assert.equal(
-                endpointBuilder.buildBaseEndpoint('http://target/', '/resource', []),
+                endpointBuilder.build('http://target/', '/resource', []),
                 'http://target/resource');
         });
 
     });
+
+    describe('buildWithParameters()', () => {
+
+        before(() => {
+            sinon.stub(paramMappings, 'get', function() {return 'a';});
+        });
+
+        after(() => {
+            paramMappings.get.restore();
+        });
+
+        let mockStringParam = { type: function() {return typeof('string');} }
+
+        it('should return a parsed uri rith a param replaced', () => {
+            assert.equal(
+                endpointBuilder.build(undefined, '/resource/{testParam}', [mockStringParam]),
+                'http://localhost:8080/resource/a');
+        });
+
+    });
+
 });
