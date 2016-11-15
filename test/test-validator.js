dec45425baae31ebc76a72dd1fba47547f6b5ce3
joanvila <vila.joan94@gmail.com>
2016-11-15 22:49:49 +0100

    Real code coverage

---
+++ test/test-validator.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const assert = require('chai').assert;
+const endpointBuilder = require('../lib/validator');
+
+const sinon = require('sinon');
+
+describe('validator', () => {
+    it('should validate', () => {});
+});
