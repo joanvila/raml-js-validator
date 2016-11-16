fca82c128bd34c488a4ab96cc29b9aa2f66dff3f
joanvila <vila.joan94@gmail.com>
2016-11-16 20:45:37 +0100

    Testing the validator object

--- test/test-validator.js
+++ test/test-validator.js
@@ -1,10 +1,69 @@
 'use strict';
 
-const assert = require('chai').assert;
-const endpointBuilder = require('../lib/validator');
+const Q = require('q');
 
+const chai = require('chai');
 const sinon = require('sinon');
+const sinonChai = require("sinon-chai");
+
+chai.should();
+chai.use(sinonChai);
+
+const Validator = require('../lib/validator');
 
 describe('validator', () => {
-    it('should validate', () => {});
+
+    const apiMock = {
+        resources: function() {
+            return [
+                {
+                    allUriParameters: function() {
+                        return ['param'];
+                    },
+                    methods: function() {
+                        return [
+                            {
+                                method: function() {
+                                    return 'get';
+                                }
+                            }
+                        ];
+                    }
+                }
+            ];
+        }
+    };
+
+    let validator = new Validator(apiMock, undefined);
+
+    before(() => {
+
+        sinon.stub(
+            validator.endpointBuilder,
+            'buildUnparsedUri',
+            function() {return 'prettyUrl';});
+
+        sinon.stub(
+            validator.endpointBuilder,
+            'parseUriWithParameters',
+            function() {return 'prettyParsedUrl';});
+
+        sinon.stub(
+            validator.endpointChecker,
+            'check',
+            function() {return Q.defer().promise});
+    });
+
+    after(() => {
+        validator.endpointBuilder.buildUnparsedUri.restore();
+        validator.endpointBuilder.parseUriWithParameters.restore();
+        validator.endpointChecker.check.restore();
+    });
+
+    it('should validate', () => {
+        validator.validate();
+        validator.endpointChecker.check.should.have.been.calledWith('prettyParsedUrl', 'get');
+        // TODO: Check number of calls
+    });
+
 });
