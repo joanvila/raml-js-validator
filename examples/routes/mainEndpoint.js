f31b5652136ddf5fd3d564a29a8ebee2a0afdc16
joanvila <joan.vila@skyscanner.net>
2016-11-20 14:03:12 +0100

    Checking query parameters too

--- examples/routes/mainEndpoint.js
+++ examples/routes/mainEndpoint.js
@@ -40,4 +40,16 @@ router.get('/task', (req, res, next) => {
     res.status(200).json('ok');
 });
 
+// Endpoints with query parameters
+
+router.get('/url/with/params', (req, res, next) => {
+    let date_in = req.param('date_in');
+    let date_out = req.param('date_out');
+    if (date_in && date_out) {
+        res.status(200).json([date_in, date_out]);
+    } else {
+        res.status(400).json('parameters not ok');
+    }
+});
+
 module.exports = router;
