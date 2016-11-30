4c56e74df7a43dd468de9df248600a0cdc3ca20a
joanvila <joan.vila@skyscanner.net>
2016-11-30 18:46:04 +0100

    More yargs (in progress)

--- bin/raml-validate.js
+++ bin/raml-validate.js
@@ -10,31 +10,62 @@ const yargs = require('yargs');
 const Validator = require('../lib/validator');
 
 const argv = yargs
-    .usage('Usage:\n  raml-validate.js </path/to/raml> [target]' +
-    '\n\nExample:\n  ' + 'raml-validate definition.raml --target http://localhost:8080')
+    .usage('Usage:\n  raml-validate.js </path/to/raml> [options]')
+
+    .help('h')
+    .alias('h', 'help')
+
+    .option('t', {
+        alias: 'target',
+        demand: false,
+        describe: 'The endpoint to test',
+        type: 'string'
+    })
+
+    .option('r', {
+        alias: 'validate-response',
+        demand: false,
+        describe: 'Validate the response schema',
+        type: 'boolean'
+    })
+
+    .option('d', {
+        alias: 'depth-levels',
+        demand: false,
+        describe: 'Response validation depth levels',
+        type: 'number'
+    })
+
+    .example('raml-validate definition.raml', '--target http://localhost:8080')
+    .example('raml-validate definition.raml', '--validate-response --depth-levels 1')
+
     .check(argv => {
         if (argv._.length < 1) {
             throw new Error('raml-validate.js: Must specify path to RAML file');
         }
         return true;
     })
-    .epilog("Website:\n  " + 'https://github.com/joanvila/raml-js-validator')
+
+    .epilog('More info and docs:\n' +
+    '  ' + 'https://github.com/joanvila/raml-js-validator')
     .argv;
+
+console.log(argv);
 
 const fileName = path.resolve(process.cwd(), argv._[0]);
 
-let api = null;
-
-try {
-    api = raml.loadApiSync(fileName, {rejectOnErrors: true});
-} catch (err) {
-    err.parserErrors.forEach((parserError) => {
-        console.log('Parsing error: ' + parserError.message);
-    });
-    throw err.message;
-}
-
-console.log('RAML parsing success. Querying api now...');
-
-const validator = new Validator(api, argv.target);
-validator.validate();
+// let api = null;
+//
+// try {
+//     api = raml.loadApiSync(fileName, {rejectOnErrors: true});
+// } catch (err) {
+//     err.parserErrors.forEach((parserError) => {
+//         console.log('Parsing error: ' + parserError.message);
+//     });
+//     throw err.message;
+// }
+//
+// console.log('RAML parsing success. Querying api now...');
+//
+// const validator = new Validator(api, argv.target);
+// validator.validate();
